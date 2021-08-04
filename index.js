ff70b7cf69d8b74248caba3f326c0f982ae180ac
hidesuke <yusuke.hideshima@ghelia.com>
2021-08-04 16:23:50 +0900

    Getting Race Result of 2021 Takarazuka

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const target = 'https://db.netkeiba.com/race/202109030411/';
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(target);
-    
+    const raseResultTable = await page.$x('//*[@id="contents_liquid"]/table');
+    const rows = await raseResultTable[0].$$('tr');
+    for(const row of rows) {
+        console.log((await (await row.getProperty('textContent')).jsonValue()).split('\n').join(','));
+    }
     await browser.close();
 })();
